be39b558a13636f5ac3f2339f158c1aa7ccf84cd
Atharva Narkhede <144204822+AtharvaNarkhede1@users.noreply.github.com>
2025-06-18 19:27:00 +0530

    Update LoginView.jsx

--- Lost-and-Found-Java-main/lostandfound1/src/components/LoginView.jsx
+++ Lost-and-Found-Java-main/lostandfound1/src/components/LoginView.jsx
@@ -53,7 +53,7 @@ export default function LoginView({ onLoginSuccess }) {
   const handleRegisterChange = (e) => {
     setRegisterForm((f) => ({ ...f, [e.target.name]: e.target.value }));
   };
-const API_BASE = process.env.REACT_APP_API_URL || "https://lost-and-found-java-mern-1.onrender.com/api";
+const API_BASE = process.env.REACT_APP_API_URL || "https://lost-and-found-java-mern.onrender.com/api";
 
   // Handle login
   const handleLogin = async (e) => {
